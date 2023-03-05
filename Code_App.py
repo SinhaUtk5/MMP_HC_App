4a8b63a0ab9c36cfae32b9bfe6f49ce5e8b2150b
SinhaUtk5 <118255174+SinhaUtk5@users.noreply.github.com>
2023-03-04 18:16:33 -0600

    Update Code_App.py

--- Code_App.py
+++ Code_App.py
@@ -34,7 +34,8 @@ st.header("Department of Petroleum Engineering: Interaction of Phase Behavior an
 st.markdown('<style>h2{color: red;}</style>', unsafe_allow_html=True)
 st.subheader("Product Description - Calculates the Minimum Miscibility Pressure (psia) for CH4 dominant hydrocarbon gas injection")
 # st.subheader("Download Input Template File")
-st.markdown("[Download Input Template](https://drive.google.com/file/d/1HNyZjobmTEBcWfk0C2cmClQfahTONrX1/view?usp=sharing)", unsafe_allow_html=True)
+st.markdown("[Download Input Template](https://drive.google.com/file/d/1HNyZjobmTEBcWfk0C2cmClQfahTONrX1/view?usp=sharing)",<style> .font {
+font-size:50px}, unsafe_allow_html=True)
 
 #st.write(df)
 pickle_in = open('finalized_MMP_HC_model2.pkl', 'rb')
